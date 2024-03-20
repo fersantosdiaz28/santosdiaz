ef93f9f6e72e723c971f0088d80df5931a9f4492
fersantosdiaz28 <161618091+fersantosdiaz28@users.noreply.github.com>
2024-03-20 10:49:06 -0600

    Add files via upload

---
+++ Conejo.cpp
@@ -0,0 +1,33 @@
+// Nombre del alumno: Santos Díaz Fernando Emmanuel
+// Grupo: 1TM13
+
+#include <iostream>
+
+float numConej = 0;
+float AlimTotal = 0;
+
+
+using namespace std;
+
+int main()
+{
+	cout << "¿Cuantos conejos hay?" <<endl;
+	cin>> numConej;
+	AlimTotal = numConej * 50;
+	cout << "Los conejos comen " << AlimTotal << "gramos. ";
+
+
+}
+ 
+
+
+
+// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
+// Depurar programa: F5 o menú Depurar > Iniciar depuración
+
+// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
+//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
+//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
+//   4. Use la ventana Lista de errores para ver los errores
+//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
+//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
